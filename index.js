7ffba20460c3dc187534a201751a36aebb47af77
Praveen Garg <praveen.garg@nerdapplabs.com>
2018-01-20 20:59:24 +0530

    fix foreach return issue

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 
-var counter = 0;
-var discarded = 0;
-
 var blockCounter = 0;
 var blockDiscarded = 0;
 
@@ -27,26 +24,7 @@ router.get('/', function(req, res) {
     res.json({ message: 'Welcome to Fog(Test)Cloud! :]' });   
 });
 
-router.route('/message')
-    
-    // create a reading (accessed at POST http://localhost:8118/ingress/message)
-    .post(function(req, res) {
-        var read = {};
-        read =  req.body;
-        isValid = validateReading(read);
-        if (! isValid) {
-            discarded ++
-            console.log("Invalid Payload", read)
-            res.json({"error": "Invalid Payload: " + discarded});
-            return;
-        }
-        counter++
-        console.log("Valid payload", read)
-        res.json({"received": "Valid Payload: " + counter});
-    });
-
-router.route('/messages')
-    
+router.route('/messages')    
     // create a reading (accessed at POST http://localhost:8118/ingress/messages)
     .post(function(req, res) {
         var readingBlock =  req.body;
@@ -65,42 +43,16 @@ router.route('/messages')
 // all of our routes will be prefixed with /api
 app.use('/ingress', router);
 
-function validateReading(readingPayload) {    
-    isValid = readingPayload.asset === undefined ? false : true;
-    // console.log("asset", isValid);
-    if (! isValid) { return isValid}
-
-    isValid = readingPayload.timestamp === undefined ? false : true;
-    // console.log("timestamp", isValid);
-    if (! isValid) { return isValid}
-
-    isValid = readingPayload.key === undefined ? false : true;
-    // console.log("key", isValid);
-    if (! isValid) { return isValid}
-    
-    readings = readingPayload.readings === undefined ? false : true;
-    // console.log("readings", readings);
-    sensor_values = readingPayload.sensor_values === undefined ? false : true;
-    // console.log("sensor_values", sensor_values);
-    isValid = readings || sensor_values
-    // console.log("readings || sensor_values", isValid);
-    if (! isValid) { return isValid}
-
-    /*
-        # check a valid timestamp?
-        # check key is UUID?  
-        # readings should be valid dict/json?
-    */
-    return true
-}
-
 function validateReadingBlock(readingBlockPayload) {
 
     isValid = (readingBlockPayload instanceof Array) ? true : false;
     if (! isValid) { return isValid}
-
     
-    readingBlockPayload.forEach(function(readingPayload) {
+    var readingPayload;
+    for (i in readingBlockPayload)
+    {
+        readingPayload = readingBlockPayload[i];
+
         isValid = readingPayload.asset_code === undefined ? false : true;
         if (! isValid) { return isValid}
 
@@ -112,19 +64,19 @@ function validateReadingBlock(readingBlockPayload) {
         isValid = (reads instanceof Array) ? true : false;
         if (! isValid) { return isValid}
 
-        reads.forEach(function(r) {
+        var r;
+        for (j in reads) {
+            r = reads[j]
+
             isValid = r.read_key === undefined ? false : true;
-            if (! isValid) { return isValid}
+            if (! isValid) { return isValid};
 
             isValid = r.reading === undefined ? false : true;
-            if (! isValid) { return isValid}
+            if (! isValid) { return isValid};
 
             isValid = r.timestamp === undefined ? false : true;
-            if (! isValid) { return isValid}
-
-        }, this);
-    }, this); 
-
-    
-    return true
+            if (! isValid) { return isValid};
+        }
+    }
+    return true;
 }
--- test.js
+++ test.js
@@ -1,21 +1,5 @@
 const axios = require('axios')
 
-
-function sendHttp(payload) {
-   // Send a POST request
-   axios({
-    method: 'POST',
-    url: 'http://localhost:8118/ingress/message',
-    data: payload
-  })
-  .then(function (response) {
-    console.log(response.data);
-  })
-  .catch(function (error) {
-    console.log(error.data);
-  });
-}
-
 function sendBlockHttp(payload) {
    // Send a POST request
    axios({
@@ -31,28 +15,7 @@ function sendBlockHttp(payload) {
   });
 }
 
-function preparePayloads() {
-    
-    valid_1 = {"timestamp": "2017-01-02T01:02:03.23232Z-05:00", "asset": "pump1", "key": "80a43623-ebe5-40d6-8d80-3f892da9b3b4", "readings": {"velocity": "500", "temperature": {"value": "32", "unit": "kelvin"}}};
-
-    valid_2 = {"timestamp": "2017-01-02T01:02:03.23232Z-05:00", "asset": "pump1", "key": "80a43623-ebe5-40d6-8d80-3f892da9b3b4", "sensor_values": {"velocity": "500", "temperature": {"value": "32", "unit": "kelvin"}}};
-
-    invalid_1 = {"timestam": "2017-01-02T01:02:03.23232Z-05:00", "asset": "pump1", "key": "80a43623-ebe5-40d6-8d80-3f892da9b3b4", "readings": {"velocity": "500", "temperature": {"value": "32", "unit": "kelvin"}}};
-
-    invalid_2 = {"timestamp": "2017-01-02T01:02:03.23232Z-05:00", "asse": "pump1", "key": "80a43623-ebe5-40d6-8d80-3f892da9b3b4", "readings": {"velocity": "500", "temperature": {"value": "32", "unit": "kelvin"}}};
-
-    invalid_3 = {"timestamp": "2017-01-02T01:02:03.23232Z-05:00", "asset": "pump1", "ke": "80a43623-ebe5-40d6-8d80-3f892da9b3b4", "readings": {"velocity": "500", "temperature": {"value": "32", "unit": "kelvin"}}};
-
-    invalid_4 = {"timestamp": "2017-01-02T01:02:03.23232Z-05:00", "asset": "pump1", "key": "80a43623-ebe5-40d6-8d80-3f892da9b3b4", "reading": {"velocity": "500", "temperature": {"value": "32", "unit": "kelvin"}}};
-    
-    payloads = new Array(valid_1, valid_2, invalid_1, invalid_2, invalid_3, invalid_4);
-
-    return payloads;
-}
-
-function prepareBlockPayloads() {
-    
-   
+function prepareBlockPayloads() {  
     valid_1 = [{ "asset_code": "TI Sensor Tag/temperature",
     "readings": [
         {
@@ -61,7 +24,7 @@ function prepareBlockPayloads() {
                 "temperature": 23,
                 "humidity": 6
                },
-               "user_ts": "2017-10-11 15:10:51.927191906"
+            "timestamp": "2017-10-11 15:10:51.927191906"
            },
            {
                "read_key": "78f73c9f-bc11-4b8d-a246-58863adf66b5",
@@ -80,7 +43,7 @@ function prepareBlockPayloads() {
            "reading": {
                "blah1": 23
               },
-              "user_ts": "2017-10-11 15:10:51.927191907"
+            "timestamp": "2017-10-11 15:10:51.927191907"
           },
           {
               "read_key": "78f73c9f-bc11-4b8d-a246-58863adf66a5",
@@ -92,12 +55,14 @@ function prepareBlockPayloads() {
           }
       ]
   },]
-    invalid_1 = {"asset_cod": "TI Sensor Tag/temperature", "readings": [{"read_key": "f1cfff7a-3769-4f47-9ded-00f0975d66f5", "reading": {"temperature": 41, "humidity": 88}, "timestamp": "2017-10-11 15:10:51.927191906"}]}
-    invalid_2 = {"asset_code": "TI Sensor Tag/temperature", "reading": [{"read_key": "f1cfff7a-3769-4f47-9ded-00f0975d66f5", "reading": {"temperature": 41, "humidity": 88}, "timestamp": "2017-10-11 15:10:51.927191906"}]}
-    invalid_3 = {"asset_code": "TI Sensor Tag/temperature", "readings": {"read_key": "f1cfff7a-3769-4f47-9ded-00f0975d66f5", "reading": {"temperature": 41, "humidity": 88}, "timestamp": "2017-10-11 15:10:51.927191906"}}
+    invalid_1 = {"asset_code": "TI Sensor Tag/temperature", "readings": [{"read_key": "f1cfff7a-3769-4f47-9ded-00f0975d66f5", "reading": {"temperature": 41, "humidity": 88}, "timestamp": "2017-10-11 15:10:51.927191906"}]}
+    invalid_2 = {"asset_code": "TI Sensor Tag/temperature", "readings": [{"read_key": "f1cfff7a-3769-4f47-9ded-00f0975d66f5", "reading": {"temperature": 41, "humidity": 88}, "timestamp": "2017-10-11 15:10:51.927191906"}]}
+
+    invalid_3 = {"asset_code": "TI Sensor Tag/temperature", "reading": [{"read_key": "f1cfff7a-3769-4f47-9ded-00f0975d66f5", "reading": {"temperature": 41, "humidity": 88}, "timestamp": "2017-10-11 15:10:51.927191906"}]}
+    invalid_4 = {"asset_code": "TI Sensor Tag/temperature", "readings": {"read_key": "f1cfff7a-3769-4f47-9ded-00f0975d66f5", "reading": {"temperature": 41, "humidity": 88}, "timestamp": "2017-10-11 15:10:51.927191906"}}
 
 
-    invalid_4 = { "asset_code": "TI Sensor Tag/temperature",
+    invalid_5 = { "asset_code": "TI Sensor Tag/temperature",
      "readings": [
          {
              "read_key": "f1cfff7a-3769-4f47-9ded-00f0975d66f5",
@@ -117,19 +82,12 @@ function prepareBlockPayloads() {
             }
         ]
     }
-    invalid_5 = {"readings": [{"read_key": "f1cfff7a-3769-4f47-9ded-00f0975d66f5", "reading": {"temperature": 41, "humidity": 88},}]}
-    payloads = new Array(valid_1, invalid_1, invalid_2, invalid_3, invalid_4, invalid_5);
-
+    invalid_6 = {"readings": [{"read_key": "f1cfff7a-3769-4f47-9ded-00f0975d66f5", "reading": {"temperature": 41, "humidity": 88},}]}
+    payloads = new Array(valid_1, invalid_1, invalid_2, invalid_3, invalid_4, invalid_5, invalid_6);
     return payloads;
 }
 
 function run() {
-    // payloads = preparePayloads();
-
-    // payloads.forEach(function(p) {
-    //     sendHttp(p); 
-    // }, this);
-
     blockPayloads = prepareBlockPayloads();
 
     blockPayloads.forEach(function(p) {
@@ -137,6 +95,5 @@ function run() {
     }, this);
        
 }
-
 
 run();
